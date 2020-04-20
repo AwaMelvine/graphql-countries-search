facd3fee23d40e36072ce48be60d93cd0c626eee
Awa Melvine <melvineawa@gmail.com>
2020-04-20 22:04:36 +0100

    Avoid duplicates in country list

--- src/context/countriesReducer.js
+++ src/context/countriesReducer.js
@@ -1,11 +1,15 @@
-import { SET_NEW_COUNTRIES, REMOVE_COUNTRY } from './types';
+import { REMOVE_COUNTRY, SET_NEW_COUNTRY } from './types';
 
 const initialState = [];
 
 export const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_NEW_COUNTRIES:
-      return [...action.payload, ...state];
+    case SET_NEW_COUNTRY:
+      const existingCountryCodes = state.map((ctry) => ctry.code);
+      if (existingCountryCodes.includes(action.payload.code)) {
+        return state;
+      }
+      return [action.payload, ...state];
     case REMOVE_COUNTRY:
       return state.filter((country) => country.code !== action.payload);
     default:
